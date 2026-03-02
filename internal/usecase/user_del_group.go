5d8ff2341fbde79379a97143985af1deac841f3a
saygik <i.krapivin@live.ru>
2026-03-02 11:39:58 +0300

    Переделка доступа 3

--- internal/usecase/user_del_group.go
+++ internal/usecase/user_del_group.go
@@ -6,24 +6,6 @@ import (
 	"github.com/saygik/go-userinfo/internal/entity"
 )
 
-func (u *UseCase) DelUserGroup(perms entity.Permissions, user string, id int) error {
-
-	if !perms.IsSysAdmin {
-		return u.Error("у вас нет прав для выполнения операции")
-	}
-	if user == "" {
-		return u.Error("пользователь не определён")
-	}
-	if !isEmailValid(user) {
-		return u.Error("неправильное имя пользователя")
-	}
-	err := u.repo.DelUserGroup(user, id)
-	if err != nil {
-		return u.Error(fmt.Sprintf("ошибка MySQL: %s", err.Error()))
-	}
-	return nil
-}
-
 func (u *UseCase) DelUserRole(perms entity.Permissions, user string, id int) error {
 
 	if !perms.IsSysAdmin {
@@ -36,6 +18,42 @@ func (u *UseCase) DelUserRole(perms entity.Permissions, user string, id int) err
 		return u.Error("неправильное имя пользователя")
 	}
 	err := u.repo.DelUserRole(user, id)
+	if err != nil {
+		return u.Error(fmt.Sprintf("ошибка MySQL: %s", err.Error()))
+	}
+	return nil
+}
+
+func (u *UseCase) DelUserSection(perms entity.Permissions, user string, section string) error {
+
+	if !perms.IsSysAdmin {
+		return u.Error("у вас нет прав для выполнения операции")
+	}
+	if user == "" {
+		return u.Error("пользователь не определён")
+	}
+	if !isEmailValid(user) {
+		return u.Error("неправильное имя пользователя")
+	}
+	err := u.repo.DelUserSection(user, section)
+	if err != nil {
+		return u.Error(fmt.Sprintf("ошибка MySQL: %s", err.Error()))
+	}
+	return nil
+}
+
+func (u *UseCase) DelUserDomainRole(perms entity.Permissions, user string, domain string) error {
+
+	if !perms.IsSysAdmin {
+		return u.Error("у вас нет прав для выполнения операции")
+	}
+	if user == "" {
+		return u.Error("пользователь не определён")
+	}
+	if !isEmailValid(user) {
+		return u.Error("неправильное имя пользователя")
+	}
+	err := u.repo.DelUserDomainRole(user, domain)
 	if err != nil {
 		return u.Error(fmt.Sprintf("ошибка MySQL: %s", err.Error()))
 	}
